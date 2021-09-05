e5c4c319719796227ce1f45a0eb8f3283c1093f8
gyan <gyan9427@gmail.com>
2021-09-05 16:13:29 +0530

    initial setup

---
+++ core/Application.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\core;
+
+/**
+ * Class Application
+ * 
+ * @author gyan
+ * @package app\core;
+ */
+
+ class Application
+ {
+    public Router $router;
+     public function __construct()
+     {
+
+     }
+ }
---
+++ core/Router.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\core;
+
+/**
+ * Class Application
+ * 
+ * @author gyan
+ * @package app\core;
+ */
+
+ class Router
+ {
+     
+     public function __construct()
+     {
+         
+     }
+ }
